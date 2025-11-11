de78b1b24689ac2fc723283411dc78bfb9292204
Mikael Todd <100557140+morteck@users.noreply.github.com>
2025-11-11 16:36:05 -0600

    Delete frontend/src/components directory

--- frontend/src/components/Console.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Console({ output }) {
-  return (
-    <div className="bg-black text-green-400 p-2 h-40 overflow-auto font-mono">
-      {output || "Console output will appear here"}
-    </div>
-  );
-}
